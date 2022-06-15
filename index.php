c2e2d597567aac7292b080b3466ca8bfc2b617d4
EliteTe4m <107521211+EliteTe4m@users.noreply.github.com>
2022-06-14 22:01:41 -0500

    primer despliegue
    V1.0

---
+++ index.php
@@ -0,0 +1,143 @@
+<php ?> 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+           <!-- Favicon-->
+           <link rel="shortcut icon" href="assets/img/logo.ico"/>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="CTM_GoatSan" />
+        <title>EliteTeam</title>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- bootstrap icons css-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+        
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/main.css" rel="stylesheet">
+        <link href="css/font.css" rel="stylesheet">
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">INICIO</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#video">Presentación</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+                   <!--     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="https://wa.me/573046500349">Contact</a></li> -->
+                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="https://www.facebook.com/EliteTe4m" target="_blank">Contactanos</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5" src="assets/img/logo2.jpeg" alt="..." />
+               <style> img.masthead-avatar {
+                    border-radius: 50%;
+                  }</style>
+
+                <!-- Masthead Heading-->
+
+                <!-- Masthead Subheading-->
+
+                <p class="masthead-subheading font-weight-light mb-0">Unete a nuestros negocios digitales y cumple tus sueños.</p>
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+            </div>
+        </header>
+        <!-- social bar-->
+
+        <div class="social-bar">
+            <a href="https://www.facebook.com/EliteTe4m" class="icon icon-facebook" target="_blank"></a>
+            <a href="https://www.youtube.com/watch?v=FIp1D2P4s3E" class="icon icon-youtube" target="_blank"></a>
+           <!--  <a href="" class="icon icon-twitter" target="_blank"></a>
+           <a href="" class="icon icon-instagram" target="_blank"></a> --> 
+          </div>
+          <!--Social bard-->
+        <!-- Portfolio Section-->
+       
+        <section class="page-section bg-primary text-white mb-0" id="video">
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <h2 class="page-section-heading text-center text-uppercase text-white">¿COMO FUNCIONA?</h2>
+                    <!-- Icon Divider-->
+                    <div class="divider-custom divider-light">
+                        <div class="divider-custom-line"></div>
+                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                        <div class="divider-custom-line"></div>
+                    </div>
+                    <!-- Footer Location-->
+        
+                    <iframe width="560" height="600" src="https://www.youtube.com/embed/FIp1D2P4s3E" title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                
+                      
+                    </div>
+
+                </section>
+                    <!-- Footer Social Icons-->
+        </footer>
+             <!-- About Section-->
+             <section class="page-section bg-primary text-white mb-0" id="about">
+                <div class="container">
+                    <!-- About Section Heading-->
+                    <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                    <!-- Icon Divider-->
+                    <div class="divider-custom divider-light">
+                        <div class="divider-custom-line"></div>
+                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                        <div class="divider-custom-line"></div>
+                    </div>
+                    <!-- About Section Content-->
+                    <div class="row">
+                        <div class="col-lg-4 ms-auto"><p class="lead">No dejes que los bancos trabajen y acaben con tus ahorros.
+                            <br>
+                            Inversiones digitales de alto rendimeinto, forex, trading, divisas, crypto activos. 
+                            </p>
+                        </div>
+                        <div class="col-lg-4 me-auto"><p class="lead">
+                         Aprende a trabajar con dinero digital comodamente a traves de tu celular, o computador ¿que esperas para invertir y ser parte de este gran equipo? <i class="bi bi-emoji-wink"></i>
+                            </p>
+                            </div>
+                    </div>
+                 
+            </section>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; EliteTeam 2022</small></div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
+</php>
